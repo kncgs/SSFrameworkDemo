e45249ec78f1f8146325e973fd97ab6ec97bdc7f
houdongbing <dongbinghouy@gmail.com>
2021-04-27 15:57:27 +0800

    Initial Commit

---
+++ SSFrameworkSDK/SSFrameworkSDK.h
@@ -0,0 +1,18 @@
+//
+//  SSFrameworkSDK.h
+//  SSFrameworkSDK
+//
+//  Created by DBH on 2021/4/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSFrameworkSDK.
+FOUNDATION_EXPORT double SSFrameworkSDKVersionNumber;
+
+//! Project version string for SSFrameworkSDK.
+FOUNDATION_EXPORT const unsigned char SSFrameworkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSFrameworkSDK/PublicHeader.h>
+
+
